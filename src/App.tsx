19906c141a94e675f669fc6373785361a0cc2637
Faidunni <hazeezfaizat@gmail.com>
2024-11-28 19:45:42 +0000

    Routing-correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import VendorProfile from "./vendor-management/Pages/VendorProfile";
 import Vendor from "./vendor-management/Vendor";
 import { Routes, Route } from "react-router-dom";
 
--- src/vendor-management/Vendor.tsx
+++ src/vendor-management/Vendor.tsx
@@ -4,13 +4,10 @@ import OrderManagement from "./Pages/OrderManagement";
 import ProductManagement from "./Pages/ProductManagement";
 import VendorProfile from "./Pages/VendorProfile";
 import SaleAnalytics from "./Pages/SaleAnalytics";
-// import { Sidebar } from "./Header/SideBar";
 
 function Vendor() {
   return (
     <main className="bg-[#E6E6E6]">
-      <Navbar />
-      {/* <Sidebar /> */}
       <Routes>
         <Route
           path="vendor-management/order-management"
