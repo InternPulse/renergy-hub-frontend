c7ed4b6928723df37503b46c5860cd6fcade017b
Denovan <146472803+denovan6k@users.noreply.github.com>
2024-11-29 12:05:55 +0100

    breadcrumbs feat

---
+++ src/products-listing/components/ui-sections/headBreadCrumbs.tsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom"
+import { ChevronsRight, ArrowLeft } from 'lucide-react'
+import { Button } from "../../../components/ui/button"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "../../../components/ui/breadcrumb"
+
+
+export default function BreadcrumbNav() {
+  return (
+    <div className="flex items-center gap-4 p-4 text-sm">
+      <Button variant="ghost" size="sm" className="gap-2" asChild>
+        <Link to="/products">
+          <ArrowLeft className="h-4 w-4" />
+          Back
+        </Link>
+      </Button>
+      <Breadcrumb>
+        <BreadcrumbList className="">
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+            
+     <Link to="/products">Productss</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <ChevronsRight className="h-4 w-4" />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+            
+            <Link to="/products/categories">Categories</Link></BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <ChevronsRight className="h-4 w-4" />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbLink  asChild>
+            
+            <Link to="/products/categories/solar-panels">Solar Panels</Link></BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <ChevronsRight className="h-4 w-4" />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbPage>Fireman 380W Solar Panel</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  )
+}
+
