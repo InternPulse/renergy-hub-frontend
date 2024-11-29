d28bb9c683f3bc568597cbd1f49efdc28a5f084b
Adeyemi Fathia <fathiaomolara02@gmail.com>
2024-11-29 20:31:20 +0100

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 
 //import "./App.css";
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./pages/landingPage"
 import { Routes, Route } from "react-router-dom";
-import CheckoutHomePage from "./pages/CheckoutHome";
+import CheckoutHomePage from "./pages/checkoutHome";
 import { VendorDashboard } from "./pages/vendorDashboard/VendorDashboard";
 import Auth from "./authentication/auth";
-import Vendor from "./vendor-management/Vendor";
+import Vendor from "./vendor-management/vendor";
 import Product from "./products-listing/page/products";
 import SettingsRoutes from "./components/settingsDashboard/SettingsLayout";
 import ShippingAndTracking from './pages/shippingAndTracking';
