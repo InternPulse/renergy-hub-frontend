b50bcb6c7f258f4b6fcbc91760ae772aa650af9d
Denovan <146472803+denovan6k@users.noreply.github.com>
2024-11-29 09:12:04 +0100

    style

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-
-import path from "path";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-=======
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
->>>>>>> 7b858b62a5f44deb9d0d7c874a49897957daa064
 
 export default defineConfig({
   plugins: [react()],
@@ -16,8 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-<<<<<<< HEAD
-});
-=======
 })
->>>>>>> 7b858b62a5f44deb9d0d7c874a49897957daa064
