3db3e7839a92e6089b6e5457147c2b5a356265f9
Awenes <63660000+Awenes@users.noreply.github.com>
2024-12-04 13:01:47 +0100

    Overview Page (Incomplete)

---
+++ src/pages/vendorcomponents/CurrentOffer.tsx
@@ -0,0 +1,13 @@
+
+
+const CurrentOffer = () => {
+  return (
+    <div className="w-[39%] h-auto bg-white rounded-lg p-2">
+      <h1 className="text-xl">
+        Current Offers
+      </h1>
+    </div>
+  )
+}
+
+export default CurrentOffer
---
+++ src/pages/vendorcomponents/TopSelling.tsx
@@ -0,0 +1,13 @@
+
+
+const TopSelling = () => {
+  return (
+    <div className="w-[59%] h-auto bg-white rounded-lg p-2">
+      <h1 className="text-xl">
+        Top Selling Products
+      </h1>
+    </div>
+  )
+}
+
+export default TopSelling
