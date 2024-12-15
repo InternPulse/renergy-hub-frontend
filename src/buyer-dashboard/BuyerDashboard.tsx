52a21c5898f745db27626d18f0a91fa087bb3270
Kehinde Balogun <balogunkehinde3@gmail.com>
2024-12-15 03:39:09 -0500

    refactored buyer dashboard layout

--- src/buyer-dashboard/BuyerDashboard.tsx
+++ src/buyer-dashboard/BuyerDashboard.tsx
@@ -1,25 +1,42 @@
-
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import BuyerOverview from "./pages/BuyerOverview";
 import BuyerSideBar from "../components/BuyerSideBar";
 import SavedProducts from './pages/SavedProducts';
 import BuyerNavbar from './components/BuyerNavbar';
 
+function BuyerLayout() {
+  return (
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <BuyerSideBar />
+
+      {/* Main Content */}
+      <div className="flex flex-col h-screen w-full">
+        {/* Navbar and Header */}
+        <BuyerNavbar />
+
+        {/* Outlet for page-specific content */}
+        <div className="flex-grow">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function BuyerDashboard() {
   return (
-      <div className="flex h-screen">
-        <BuyerSideBar/>
-        <div className='flex flex-col h-screen'>
-          <BuyerNavbar/>
-        <Routes>
-          <Route path="/" element={<BuyerOverview />} />
-          <Route path="/saved-products" element={<SavedProducts />} />
-        </Routes>
-        </div>
-      </div>
+    <Routes>
+      {/* Parent Route */}
+      <Route path="/" element={<BuyerLayout />}>
+        {/* Default Route (Overview) */}
+        <Route index element={<BuyerOverview />} />
 
-  )
+        {/* Saved Products Route */}
+        <Route path="/saved-products" element={<SavedProducts />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default BuyerDashboard
+export default BuyerDashboard;
