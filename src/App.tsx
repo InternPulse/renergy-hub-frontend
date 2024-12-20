f662ea067e4c49decadc5fb8be7f71f6c4d5fa7a
Denovan <146472803+denovan6k@users.noreply.github.com>
2024-12-20 04:21:03 +0100

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-//import "./App.css";
+
 import LandingPage from "./pages/landingPage";
 import { Routes, Route } from "react-router-dom";
 import CheckoutHomePage from "./pages/checkoutHome";
@@ -9,15 +9,15 @@ import Product from "./products-listing/page/products";
 import SettingsRoutes from "./components/SettingsDashboard/SettingsLayout";
 import ShippingAndTracking from "./pages/ShippingAndTracking";
 import ContentSection from "./sections/contentSection";
-import SecurityCompliance from "./pages/SecurityCompliance"; // From HEAD
-import AdminDashboardRoutes from "./components/AdminDashboard/AdminDashboardRoutes"; // From oo-branch
+import SecurityCompliance from "./pages/SecurityCompliance"; 
+import AdminDashboardRoutes from "./components/AdminDashboard/AdminDashboardRoutes"; 
 import AboutUs from "./pages/AboutUs";
-// import Page from "./buyer-dashboard/BuyerDashboard"
+
 import ContactUs from "./pages/ContactUs";
 import RenergyBlog from "./pages/RenergyBlog";
 import BlogDetail from "./pages/BlogDetail";
 import ProductList from "./shoppingcart/ProductList";
-//import AdminDashboard from "./components/AdminDashboard/AdminDashboard"; // From oo-branch
+
 import { Toaster } from "sonner";
 import BuyerDashboard from "./buyer-dashboard/BuyerDashboard";
 import ProtectedRoute from "./protectedRoute";
@@ -32,11 +32,9 @@ function App() {
         <Route path="/blog" element={<RenergyBlog />} />
         <Route path="/blog-detail" element={<BlogDetail />} />
         <Route path="/vendor-dashboard/*" element={<VendorDashboard />} />
-        {/* <Route path="/admin-dashboard" element={<AdminDashboard />} /> */}
-
-        {/* FROM BUYER DASHBOARD */}
+     
         <Route path="/buyer-dashboard/*" element={<BuyerDashboard />} />
-        {/* From oo-branch */}
+       
         <Route path="/checkout" element={<CheckoutHomePage />} />
         <Route path="/authentication/*" element={<Auth />} />
 
@@ -44,7 +42,7 @@ function App() {
 
         <Route path="/settings/*" element={<SettingsRoutes />} />
         <Route path="/security-compliance" element={<SecurityCompliance />} />
-        {/* From HEAD */}
+       
         <Route
           path="/shipping-and-tracking"
           element={<ShippingAndTracking />}
@@ -61,7 +59,7 @@ function App() {
         />
         <Route path="/*" element={<Vendor />} />
 
-        {/* From oo-branch */}
+   
       </Routes>
       <Toaster />
     </div>
